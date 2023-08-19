6e0c292d102df0d251abdcdd0531bdf152c64d02
Juan Martín Diez <diez.juan.martin12@gmail.com>
2023-08-18 21:35:00 -0300

    TerceraPreEntrega

--- js/home.js
+++ js/home.js
@@ -34,7 +34,7 @@ const renderProducts = () => {
         </button>
       </div>
     </div>`;
-      productsSection.querySelector(".row").appendChild(card);
+    productsSection.appendChild(card);
     });
   };
 
@@ -67,7 +67,7 @@ const renderProducts = () => {
             </button>
           </div>
         </div>`;
-        productsSection.querySelector(".row").appendChild(card);
+        productsSection.appendChild(card);
       });
     }
   });
@@ -93,7 +93,7 @@ const renderProducts = () => {
         </button>
       </div>
     </div>`;
-    productsSection.querySelector(".row").appendChild(card);
+    productsSection.appendChild(card);
   });
 };
 
